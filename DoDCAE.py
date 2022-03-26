2f55075c279af0214ff7e88fd83f82aec5ce9166
Trevon <90734292+TrevonWilkins@users.noreply.github.com>
2022-03-26 19:15:45 -0400

    DoDCAE
    Code revision, added hyper-threading.

--- DoDCAE.py
+++ DoDCAE.py
@@ -7,10 +7,13 @@ import time
 import hashlib
 from bs4 import BeautifulSoup
 from tqdm import tqdm
-
-startTime = time.time()
+from requests.sessions import Session
+from concurrent.futures import ThreadPoolExecutor
+from threading import Thread,local
+from datetime import date
 
 links = []
+thread_local = local()
 
 stock_quarters = pd.DataFrame(["Q1: 1JAN – 31MAR","Q2: 1APR – 30JUN","Q3: 1JUL – 30SEP","Q4: 1OCT – 31DEC"])
 print(stock_quarters); print()
@@ -18,31 +21,36 @@ start_date = input("Start Format (YYYY-MM-DD):"); end_date = input("End Format (
 backdate_url = f'https://www.defense.gov/News/Contracts/StartDate/{start_date}/EndDate/{end_date}/'
 backdate_urlcheck = re.findall(r'https://www.defense.gov/News/Contracts/StartDate/[0-9]{4}-[0-9]{2}-[0-9]{2}/EndDate/[0-9]{4}-[0-9]{2}-[0-9]{2}/', (backdate_url))
 
+startTime = time.time()
+
 ##Input validation
 if backdate_url == backdate_urlcheck[0]:
     backdate_url = backdate_urlcheck[0]
+    links = [backdate_url]
 
-def grab_page_links(links, backdate_url, i=2):
-    template = f'?Page={i}'
-    url = backdate_url + template
-    search = requests.get(url)
-    search = BeautifulSoup(search.text, "html.parser")
-    search = str(search.find_all('listing-titles-only'))
-    search_links = re.findall(r'http://www.defense.gov/News/Contracts/Contract/Article/[0-9]{7,7}/', (search))
+def link_constant(start_date = start_date, end_date = end_date): 
+#Link_constant =(delta.days *avg#wks_mon * max#links_wk/avg#day_mon)/ (max_#links_pg)
+    start_date = [int(x.lstrip('0')) for x in start_date.replace('-',',').split(',')]
+    end_date = [int(x.lstrip('0')) for x in end_date.replace('-',',').split(',')]
+    start_date = date(start_date[0], start_date[1], start_date[2])
+    end_date = date(end_date[0], end_date[1], end_date[2])
+    delta = end_date - start_date
+    link_constant = int ((delta.days * .715)/10)
+    return link_constant
 
-    if len(search_links) > 0:
-        i = i+1
-        [links.append(str) for str in search_links if str not in links]
-        grab_page_links(links, backdate_url, i=i)
+links.extend([f'{backdate_url}?Page={i+1}' for i in range(1,link_constant())])
+
+def grab_page_links(links,new_list=[], x=0):
+    if x == len(links):
+        return new_list
     else:
-        search = requests.get(backdate_url)
+        search = requests.get(links[x])
         search = BeautifulSoup(search.text, "html.parser")
         search = str(search.find_all('listing-titles-only'))
         search_links = re.findall(r'http://www.defense.gov/News/Contracts/Contract/Article/[0-9]{7,7}/', (search))
-        [links.append(str) for str in search_links if str not in links]
-    return links
-
-links = grab_page_links(links,backdate_url, i=2)
+        [new_list.append(str) for str in search_links if str not in new_list]
+    return grab_page_links(links, new_list = new_list, x=x+1)
+links = grab_page_links(links)
 
 def link_hash(x):
     md5 = hashlib.md5(x.encode('utf-8')).hexdigest()
@@ -50,39 +58,46 @@ def link_hash(x):
 
 link_hashes = list(map(link_hash,links))
 
-#We request the contents from each link saving bandwidth and execution time.
-def save_data(x):
-    res = requests.get(x)
-    soup2 = BeautifulSoup(res.text, "html.parser")
-    return soup2
- 
-links = list(map(save_data,tqdm(links)))
+def get_session() -> Session:
+    if not hasattr(thread_local,'session'):
+        thread_local.session = requests.Session()
+    return thread_local.session
+
+def download_link(x:str):
+    requests = get_session()
+    with requests.get(x) as response:
+        response = BeautifulSoup(response.text, "html.parser")
+    return response
+
+def download_all(urls:list) -> None:
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        return list(executor.map(download_link,links))
+        
+links = tqdm(download_all(links))
 
 def results_collection(x):
-    soup2 = x
-    for match in soup2.find_all('span'):
+    for match in x.find_all('span'):
         match.unwrap()
-    for match in soup2.find_all('a'):
+    for match in x.find_all('a'):
         match.unwrap()
-    for p in soup2.find_all('p'):
+    for p in x.find_all('p'):
         if 'style' in p.attrs:
             del p.attrs['style']
-    for p in soup2.find_all('p'):
+    for p in x.find_all('p'):
         if 'class' in p.attrs:
             del p.attrs['class']
-    results = soup2.find('div', attrs={'class':'body'}).find_all("p") #Article contents location
+    results = x.find('div', attrs={'class':'body'}).find_all("p") #Article contents location
     return results
 
 paragraphs = list(map(results_collection, links))
 
 #Creates list of how many paragraphs exists from each link that we use to pass in our para aggregation function.
-def gather_length(paragraphs):
-    count = []
-    x = 0
-    while x != len(paragraphs):
+def gather_length(paragraphs, count = [], x = 0):
+    if x == len(paragraphs):
+        return count
+    else:
         count.append(len(paragraphs[x]))
-        x = x + 1
-    return count
+    return gather_length(paragraphs, count = count, x = x+1)
 
 count = gather_length(paragraphs)
 
@@ -91,10 +106,8 @@ paragraphs = [str(item) for sublist in paragraphs for item in sublist]
 
 #Creates list of dates we encounter as we sift through each link. Used for appension to EACH paragraph.
 def contract_date(x):
-    soup2 = x
-    contract_date = str(soup2.find("meta", property="og:title")["content"]) if str(soup2.find("meta", property="og:title")["content"]) else "no date given"
+    contract_date = str(x.find("meta", property="og:title")["content"]) if str(x.find("meta", property="og:title")["content"]) else "no date given"
     contract_date = re.findall(r'[A-Z]{1,1}[a-z]{2,8}\s[0-9]{1,2},\s[0-9]{4,4}',(contract_date))[0]
-    
     return contract_date
 
 contract_date = list(map(contract_date, links))
@@ -103,15 +116,13 @@ contract_date = list(map(contract_date, links))
 def para_aggregation(paragraphs, count, contract_date, link_hashes, new_list = [], x = 0, start = 0):
     
     if x >= len(count):
-        None
+        return new_list
     else:
         end = sum(count[0:x]) + sum(count[x:(x+1)])
-        joint_string = "".join([contract_date[x], link_hashes[x][0], link_hashes[x][1]])
+        joint_string = "".join([contract_date[x], link_hashes[x][0], " ", link_hashes[x][1]])
         contents = ["".join([item, joint_string]) for item in paragraphs[start:end]]
         new_list.extend(contents)
-        x = x+1
-        para_aggregation(paragraphs, count, contract_date, link_hashes, new_list = new_list, x = x, start = end)
-    return new_list
+    return para_aggregation(paragraphs, count, contract_date, link_hashes, new_list = new_list, x = x+1, start = end)
 
 paragraphs = para_aggregation(paragraphs, count, contract_date,link_hashes)
 
@@ -132,7 +143,7 @@ def regex_run(x):
     organizations_dic = {'<p>':'','\'':'','[':'',']':'','&amp':'','The':'','Inc':'',';':'','.':'','"':''}
     for key, value in organizations_dic.items():
         organizations = ([str.replace(key, value) for str in organizations])
-
+    
     #Collect Location Data.
     loc = re.findall(r'(Alabama|Alaska|Arizona|Arkansas|Australia|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New Hampshire|New Jersey|New Mexico|New York|North Carolina|North Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Puerto Rico|Rhode Island|South Carolina|South Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West Virginia|Wisconsin|Wyoming/)', (x))
     locations.append(str(loc[0:1]))
@@ -146,7 +157,7 @@ def regex_run(x):
     month, day, year = contract_date
     month_translation = {'January':[1,'Q1'],'February':[2,'Q1'],'March':[3,'Q1'],'April':[4,'Q2'],'May':[5,'Q2'],'June':[6,'Q2'],'July':[7,'Q3'],'August':[8,'Q3'],'September':[9,'Q3'],'October':[10,'Q4'],'November':[11,'Q4'],'December':[12,'Q4']}
     quarter, month = month_translation.get(month)[1], month_translation.get(month)[0]
-
+    
     #Collect Source Link.
     link = re.findall(r'http://www.defense.gov/News/Contracts/Contract/Article/[0-9]{7,7}/', (x))
     link_hash = re.findall(r'[0-9a-fA-F]{32}', (x))
