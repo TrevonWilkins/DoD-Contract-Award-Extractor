db367225aa9eb07b6719e465f617035b1f1de195
Trevon <90734292+TrevonWilkins@users.noreply.github.com>
2022-05-30 13:45:47 -0400

    DoDCAE

--- DoDCAE.py
+++ DoDCAE.py
@@ -3,18 +3,20 @@
 # coding: utf-8
 
 import re
+import csv
 import time
 import hashlib
 import requests
 import pandas as pd
 from tqdm import tqdm
-from bs4 import BeautifulSoup
 from datetime import date
+from bs4 import BeautifulSoup
+from difflib import SequenceMatcher
 from requests.sessions import Session
 from concurrent.futures import ThreadPoolExecutor, ProcessPoolExecutor
 
-def dodcae(start_date = str, end_date = str, csv = False):
- 
+def dodcae(start_date = str, end_date = str, csvfile = False):
+    
     """
     Take a given date range and return Department of Defense
     awards within the given range.
@@ -24,11 +26,17 @@ def dodcae(start_date = str, end_date = str, csv = False):
     
     :param end_date: The end interval.
     :type number: str
+    
+    :(optional) param csvfile: Output CSV file.
+    :type boolean, default = False
 
     :return: pandas dataframe of awards disbursed
     :rtype: df
     
-    Example Command: 'Q4_2021 = dodcae(start_date="2021-10-01", end_date="2021-12-31")'
+    Example Commands:
+    'df = dodcae(start_date="2021-10-01", end_date="2021-10-06")'
+    'Q4_2021 = dodcae(start_date="2021-10-01", end_date="2021-12-31")'
+    'Q4_2021 = dodcae(start_date="2021-10-01", end_date="2021-12-31", csvfile=True)'
     """
     
     startTime = time.time()
@@ -40,9 +48,9 @@ def dodcae(start_date = str, end_date = str, csv = False):
     if backdate_url == re.findall(r'https://www.defense.gov/News/Contracts/StartDate/[0-9]{4}-[0-9]{2}-[0-9]{2}/EndDate/[0-9]{4}-[0-9]{2}-[0-9]{2}/', (backdate_url))[0]:
         links = [backdate_url]
         
-    # Algorithm that defines amount of pages on defense.gov to search
+    # Search algorithm that defines amount of pages on defense.gov to traverse
     def link_constant(start_date = start_date, end_date = end_date): 
-        #Link_constant Algorithm =(delta.days *avg#wks_mon * max#links_wk/avg#day_mon)/(max_#links_pg)
+        #Link_constant algorithm =(delta.days *avg#wks_mon * max#links_wk/avg#day_mon)/(max_#links_pg)
         start_date = [int(x.lstrip('0')) for x in start_date.replace('-',',').split(',')]
         end_date = [int(x.lstrip('0')) for x in end_date.replace('-',',').split(',')]
         start_date, end_date = date(start_date[0], start_date[1], start_date[2]), date(end_date[0], end_date[1], end_date[2])
@@ -57,7 +65,7 @@ def dodcae(start_date = str, end_date = str, csv = False):
         with ThreadPoolExecutor(max_workers=None) as executor:
             return list(executor.map(download_link,links))   
             
-    #Pulls article links from each page link, deduplicates results of the links variable to conserve bandwidth, and converts links into MD5 hash for database integrity uses.
+    #Pulls article links from each page link, deduplicates the results, and converts links into MD5 hash for database integrity use cases.
     links = {k:hashlib.md5(k.encode('utf-8')).hexdigest() for k in list(set([str for str in re.findall(r'http://www.defense.gov/News/Contracts/Contract/Article/[0-9]{7,7}/', str(download_all(links)))]))}
     
     #Grabs all paragraphs of each article, cleanses a few tags, and returns the results to paragraphs variable.
@@ -72,7 +80,7 @@ def dodcae(start_date = str, end_date = str, csv = False):
                 del p.attrs['class']
         results = [str(x) for x in x.find('div', attrs={'class':'body'}).find_all("p")]
         return contract_date, results 
-    paragraphs = {k[0]: k[1] for k in list(map(results_collection, tqdm(download_all(list(links.keys())))))}
+    paragraphs = {k[0]: k[1] for k in list(map(results_collection,tqdm(download_all(list(links.keys())))))}
     
     #Appends respective contract dates to each paragraph for database granularity.
     def para_aggregation(paragraphs = paragraphs, links = links, new_list = [], x = 0):
@@ -81,16 +89,51 @@ def dodcae(start_date = str, end_date = str, csv = False):
         else:
             new_list.extend(["".join([item, "".join([list(paragraphs.items())[x][0], list(links.items())[x][0], " ", list(links.items())[x][1]])]) for item in list(paragraphs.values())[x]])
         return para_aggregation(new_list = new_list, x = x+1)
-
-    #Regex search function to build a singular table for exportation.
-    global regex_run #configured as global variable so our multiprocessing function can see it
+        
+    try: 
+        #Link to CSV to ticker referencing dictionary.
+        url = 'https://raw.githubusercontent.com/TrevonWilkins/DoD-Contract-Award-Extractor/main/Stock%20Dictionary.csv' 
+        res = requests.get(url)
+        global stocklist, stocklist_list, stocklist_values
+        stocklist = dict(csv.reader(list(map(lambda x: x.decode('utf-8'), res.content.splitlines()))))
+        stocklist_list, stocklist_values = stocklist.items(), stocklist.values()
+    except Exception:
+        pass
+    #Takes in an organizations name as it appears on defense.gov and tries to guess the stock ticker against the stock dictionary list provided. 
+    #The likeness percentage is used to return similarities greater than 85% in order to increase guess quality if the guess is below this threshold the function will return "Ticker Not Found'
+    def ticker(a, likeness_percentage = .85, measurement=[0.0,""]):
+        try:
+            global stocklist, stocklist_values, stocklist_list
+            a = a.replace("The", '').lstrip()
+            a = a.replace("&amp", '&')
+            check = a[0:3]
+            res = [idx for idx in stocklist_values if idx.lower().startswith(check.lower())]
+            for x in res:
+                if x.startswith(("General", "Black", "United", "US", "West", "Parker", "North", "Johns", "Federal", "Huntington", "Science")):
+                    b,y = ' '.join(a.split(maxsplit=2)[:2]), ' '.join(x.split(maxsplit=2)[:2])
+                else:
+                    b,y =  a.split()[0], x.split()[0]
+                cost = float(SequenceMatcher(None,b,y).ratio())
+                if cost > measurement[0]:
+                    measurement = [float(cost),x]
+            if measurement[0] > likeness_percentage:   
+                for key, value in stocklist_list:
+                     if measurement[1] == value:
+                        return key
+            else:
+                return "Ticker Not Found"
+        except Exception:
+            return "Ticker Not Found"
+            
+    #Regex search function to build a single table for exportation.
+    global regex_run #set as global variable so our multiprocessing function can see it
     def regex_run(x):
         #Processing Dictionaries
         month_translation = {'January':[1,'Q1'],'February':[2,'Q1'],'March':[3,'Q1'],'April':[4,'Q2'],'May':[5,'Q2'],'June':[6,'Q2'],'July':[7,'Q3'],'August':[8,'Q3'],'September':[9,'Q3'],'October':[10,'Q4'],'November':[11,'Q4'],'December':[12,'Q4']}
         organizations_dic = {'<p>':'','\'':'','[':'',']':'','&amp':'','The':'','Inc':'',';':'','.':'','"':''}
         locations_dic = {'\'':'','[':'',']':''}
         monetary_dic = {'$':'',',':'','\'':'','[':'',']':''}
-        #Collect [Monetary, Organizations, Location, Month, Day, Year, Quarter, Link, and MD5 Hash Data] database fields. 
+        #Collect [Monetary, Organizations, Location, Month, Day, Year, Quarter, Approximate Ticker, Link, and MD5 Hash Data] database fields. 
         monetary = re.findall(r'(\$\d+\,\d{3}\,\d{3}\,?\d{0,3})', (x))[0:1]
         organizations = re.findall(r'^(.+?),', (x))
         locations = re.findall(r'(Alabama|Alaska|Arizona|Arkansas|Australia|California|Colorado|Connecticut|Delaware|Florida|Georgia|Hawaii|Idaho|Illinois|Indiana|Iowa|Kansas|Kentucky|Louisiana|Maine|Maryland|Massachusetts|Michigan|Minnesota|Mississippi|Missouri|Montana|Nebraska|Nevada|New Hampshire|New Jersey|New Mexico|New York|North Carolina|North Dakota|Ohio|Oklahoma|Oregon|Pennsylvania|Puerto Rico|Rhode Island|South Carolina|South Dakota|Tennessee|Texas|Utah|Vermont|Virginia|Washington|West Virginia|Wisconsin|Wyoming/)', (x))[0:1]
@@ -105,7 +148,7 @@ def dodcae(start_date = str, end_date = str, csv = False):
             organizations = "".join([str.replace(key, value) for str in organizations])
         for key, value in monetary_dic.items():
             monetary = "".join([str.replace(key, value) for str in monetary])
-        return organizations, monetary, locations, month, day, year, quarter, link, link_hash
+        return organizations, monetary, locations, month, day, year, quarter, ticker(organizations), link, link_hash
         
     with ProcessPoolExecutor(max_workers=None) as executor:
         data = list(executor.map(regex_run, para_aggregation()))
@@ -113,11 +156,12 @@ def dodcae(start_date = str, end_date = str, csv = False):
 
     #Cleanse Data and Export to CSV.
     pd.set_option('display.max_colwidth', None)
-    df = pd.DataFrame(data, columns =['Organizations', 'Monetary', 'Location', 'Month', 'Day', 'Year', 'Quarter', 'Link', 'Link MD5 Hash'])
+    df = pd.DataFrame(data, columns =['Organizations', 'Monetary', 'Location', 'Month', 'Day', 'Year', 'Quarter', 'Approximate Ticker', 'Link', 'Link MD5 Hash'])
     df[["Monetary", "Month", "Day", "Year"]] = df[["Monetary", "Month", "Day", "Year"]].apply(pd.to_numeric)
     df['Organizations'] = df['Organizations'].apply(lambda x: ' '.join(x.split(maxsplit=3)[:3]))
-    if csv == True:
-            df.to_csv(f'DoDCAE_{start_date}_{end_date}.csv', index=False)
+    
+    if csvfile == True:
+        df.to_csv(f'DoDCAE_{start_date}_{end_date}.csv', index=False)
 
     executionTime = (time.time() - startTime)
     print(); print('Execution time in seconds: ' + str(executionTime))
